54b6433204925f3490e0f2576d7424922aca2544
Lois Rilo <lois.rilo@forgeflow.com>
2022-08-30 12:03:11 +0200

    fixup! fixup! [REF] oxigen_stock_barcodes: simplify after refactor upstream

--- oxigen_stock_barcodes/wizards/stock_barcodes_read_picking.py
+++ oxigen_stock_barcodes/wizards/stock_barcodes_read_picking.py
@@ -11,9 +11,22 @@ _logger = logging.getLogger(__name__)
 class WizStockBarcodesReadPicking(models.TransientModel):
     _inherit = "wiz.stock.barcodes.read.picking"
 
-    def action_confirm_picking(self):
-        picking = self.env["stock.picking"].browse(
-            self.env.context.get("picking_id", False)
-        )
-        picking.action_confirm()
-        return self.env.ref("stock_barcodes.action_stock_barcodes_action").read()[0]
+    # WARNING: override standard method, combines `wiz.stock.barcodes.read orignal`
+    # and super at `wiz.stock.barcodes.read.picking`; both at `stock_barcodes`
+    # module.
+    def action_product_scaned_post(self, product):
+        self.package_id = False
+        if self.product_id != product and self.lot_id.product_id != product:
+            self.lot_id = False
+        self.product_id = product
+        self.product_uom_id = self.product_id.uom_id
+        self.packaging_id = self.product_id.packaging_ids[:1]
+        # START OF CHANGES
+        if not (self.manual_entry or self.is_manual_qty):
+            # Do not override qty always, we are increasing it with lot scans.
+            # See `oxigen_stock_barcodes_gs1` module and `process_lot` method.
+            self.product_qty = 1.0
+        # END OF CHANGES
+        # Super in wiz.stock.barcodes.read.picking
+        if self.auto_lot and self.picking_type_code != "incoming":
+            self.get_lot_by_removal_strategy()
---
+++ oxigen_stock_barcodes/wizards/wizard_candidate_picking.py
@@ -0,0 +1,19 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class WizCandidatePicking(models.TransientModel):
+    _inherit = "wiz.candidate.picking"
+
+    def action_confirm_picking(self):
+        picking = self.env["stock.picking"].browse(
+            self.env.context.get("picking_id", False)
+        )
+        picking.action_confirm()
+        return self.env.ref("stock_barcodes.action_stock_barcodes_action").read()[0]
