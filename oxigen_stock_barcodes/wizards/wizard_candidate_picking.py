2632714966596a2758c4503db7ed9ee32eb07403
Lois Rilo <lois.rilo@forgeflow.com>
2022-08-30 12:03:51 +0200

    fixup! fixup! fixup! [REF] oxigen_stock_barcodes: simplify after refactor upstream

--- oxigen_stock_barcodes/wizards/wizard_candidate_picking.py
+++ oxigen_stock_barcodes/wizards/wizard_candidate_picking.py
@@ -1,11 +1,7 @@
 # Copyright 2022 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-import logging
-
 from odoo import models
-
-_logger = logging.getLogger(__name__)
 
 
 class WizCandidatePicking(models.TransientModel):
